f65ca39f092bdd325f789340a0114816cc193224
Dmitriy <D@mail.ru>
2022-01-22 20:00:31 +0300

    добавил user

--- ServiceAvto/Program.cs
+++ ServiceAvto/Program.cs
@@ -6,7 +6,15 @@ namespace ServiceAvto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string? user;
+            while (true)
+            {
+                Console.WriteLine("Введите Ваш статус (администратор или клиент): ");
+                user = Console.ReadLine();
+                if (user == "Администратор" || user == "администратор" || user == "Клиент" || user == "клиент") break;
+            }
+            Console.WriteLine($"Добрый день, {user}!");
         }
     }
 }
+
